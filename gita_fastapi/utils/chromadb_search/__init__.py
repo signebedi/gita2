7eb5b1a3aaad68bcc4a0f4406410612db8352c8c
signebedi <signe@siftingwinnowing.com>
2025-01-02 21:39:31 -0600

    Added: chromadb scripts

---
+++ gita_fastapi/utils/chromadb_search/__init__.py
@@ -0,0 +1,15 @@
+from .chromadb_utils import (
+    SAMPLE_DATA,
+    get_client,
+    get_embedding_model,
+    add_data,
+    query_collection,
+)
+
+__all__ = [
+    "SAMPLE_DATA",
+    "get_client",
+    "get_embedding_model",
+    "add_data",
+    "query_collection",
+]
